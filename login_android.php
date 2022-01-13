d627da600b8c29891f3e789ade03b570e57c3f84
Mohit Gupta <guptamm.22@gmail.com>
2022-01-13 12:44:05 +0530

    User Role added

--- login_android.php
+++ login_android.php
@@ -1,17 +1,29 @@
 <?php
 $conn=mysqli_connect("localhost","root","xxx123xxx");
-mysqli_select_db($conn,"demo");
+mysqli_select_db($conn,"minor_new");
 
-$user_id=trim($_POST['user_id']);
+$computer_code=trim($_POST['computer_code']);
 $pswd=trim($_POST['password']);
 $hash_val = md5($pswd);
 
-$qry="select * from login where user_id='$user_id' and password='$hash_val'";
+
+$qry="select * from login where computer_code='$computer_code' and password='$hash_val'";
 $raw=mysqli_query($conn,$qry);
 $count=mysqli_num_rows($raw);
 
+$query="SELECT * FROM login WHERE computer_code='$computer_code' and password='$hash_val' and is_student=0";
+$rawst=mysqli_query($conn,$query);
+$countst=mysqli_num_rows($rawst);
+
+
 if (!$conn) {
     die("Database connection failed: " . mysqli_error());
+}
+
+if ($countst>0) {
+	$response['is_student']="yes";
+}else{
+	$response['is_student']="no";
 }
 
 if($count>0){
